549d2bd8d3829104f080e01debae1ddfadb4fec6
NightAngel5 <yassinetoujani5@gmail.com>
2025-03-21 03:14:56 +0100

    fonctions preliminaires

--- tools.cc
+++ tools.cc
@@ -1,3 +1,39 @@
 #include "tools.h"
 using namespace std;
 
+double norme(Cart P)
+{
+    return sqrt((P.x) *(P.x) + (P.y) *(P.y));
+}
+
+Cart conversion(Pol P)
+{
+    Cart M = {P.x * cos(P.y), P.x * sin(P.y)};
+    return M;
+}
+
+Pol conversion(Cart P)
+{
+    Pol M = {norme(P), atan2(P.x, P.y)};
+    return M;
+}
+
+double distance(Cart P1, Cart P2)
+{
+    return sqrt(pow((P1.x) - (P2.x), 2) + pow((P1.y) - (P2.y), 2));
+}
+
+double distance(Pol P1, Pol P2)
+{
+    return distance(conversion(P1), conversion(P2));
+}
+
+double distance(Cart P1, Pol P2)
+{
+    return distance(P1, conversion(P2));
+}
+
+double distance(Pol P1, Cart P2)
+{
+    return distance(P1, conversion(P2));
+}
--- tools.h
+++ tools.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cmath>
 constexpr double epsil_zero(0.5);
 
 struct S2d {
@@ -8,4 +8,10 @@ struct S2d {
 typedef S2d Cart; //x l'abscisse et y l'ordonnée
 typedef S2d Pol; //x la norme et y l'angle
 
-
+double norme(Cart P);
+Cart conversion(Pol P);
+Pol conversion(Cart P);
+double distance(Cart P1, Cart P2);
+double distance(Pol P1, Pol P2);
+double distance(Cart P1, Pol P2);
+double distance(Pol P1, Cart P2);
