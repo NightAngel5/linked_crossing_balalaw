76d578082309a03b66e246cf102855dee6a8c308
NightAngel5 <yassinetoujani5@gmail.com>
2025-03-21 02:38:05 +0100

    tool v1

---
+++ tools.h
@@ -0,0 +1,11 @@
+#include <iostream>
+constexpr double epsil_zero(0.5);
+
+struct S2d {
+    double x = 0.;
+    double y = 0.;
+};
+typedef S2d Cart; //x l'abscisse et y l'ordonnée
+typedef S2d Pol; //x la norme et y l'angle
+
+
