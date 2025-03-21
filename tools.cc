97a95ba98d681f9257e9600760a589648dc91df5
ADAM2548 <edam.belghith@gmail.com>
2025-03-21 03:15:02 +0100

    cercle et ses fonctions

--- tools.cc
+++ tools.cc
@@ -1,3 +1,26 @@
 #include "tools.h"
 using namespace std;
 
+bool inclusion (Cercle C1, Cart P){
+    if (distance(C1.C, P)<C1.R-epsil_zero) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool inclusion (Cercle C1, Cercle C2){
+    if (distance(C1.C, C2.C)<C2.R-C1.R-epsil_zero) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool intersection(Cercle C1, Cercle C2){
+    if (distance(C1.C, C2.C)<C2.R+C1.R+epsil_zero) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- tools.h
+++ tools.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cmath>
 constexpr double epsil_zero(0.5);
 
 struct S2d {
@@ -7,5 +7,11 @@ struct S2d {
 };
 typedef S2d Cart; //x l'abscisse et y l'ordonnée
 typedef S2d Pol; //x la norme et y l'angle
+struct Cercle {
+    S2d C;
+    double R;
+};
 
-
+bool inclusion (Cercle C1, Cart P);
+bool inclusion (Cercle C1, Cercle C2);
+bool intersection(Cercle C1, Cercle C2);
